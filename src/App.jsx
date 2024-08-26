8f948f72709396265204a4219e2b6805fb4c4ae1
Wellington <welltecnic@hotmail.com>
2024-08-26 11:40:41 -0300

    Criando o Nav e Footer e chamadno os elementos filhos com Outlet

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import Nav from './components/Nav'
+import Footer from './components/Footer'
+import {Outlet} from 'react-router-dom'
+
 
 function App() {
   
   return (
     <>
-      
+     <Nav/>
+      {/* CHAMANDO TODOS OS ELEMENTOS FILHOS */}
+      <Outlet/>
+     <Footer/>
     </>
   )
 }
